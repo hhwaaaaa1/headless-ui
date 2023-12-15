71c8a8d7431d2c44f8367d32491cc995661b5c4a
hhwaaaaa1 <hhwaaaaa1@naver.com>
2023-12-15 19:48:22 +0900

    Initialize typescript

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,19 +1,29 @@
-import resolve from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default {
-	input: 'src/index.js',
+	input: 'src/index.ts',
 	output: {
-		dir: 'build',
+		dir: 'dist',
 		format: 'es'
 	},
 	external: [/@babel\/runtime/],
 	plugins: [
-		resolve(),
+		commonjs(),
+		nodeResolve(),
+		typescript(),
 		babel({ 
 			babelHelpers: 'runtime',
-			presets: ["@babel/preset-env", "@babel/preset-react"],
+			presets: [
+				"@babel/preset-env",
+				"@babel/preset-react",
+				"@babel/preset-typescript"
+			],
 			plugins: ["@babel/plugin-transform-runtime"],
-		})
+		}),
+		peerDepsExternal()
 	]
 };
